eb2974dce7620385aefdccc8cb2b5d6e06e5e144
Alulestad <vladimirlestad@gmail.com>
2024-05-29 19:33:41 -0500

    taller13

--- js/app.js
+++ js/app.js
@@ -1,19 +1,70 @@
-
-
 console.log(Vue); /* este objeto tiene todas las funcionalidades de vue */
 
+const estudiantes = [
+  {
+    nombre: "Juan",
+    apellido: "Gutierres",
+  },
+  {
+    nombre: "Pedro",
+    apellido: "Pérez",
+  },
+  {
+    nombre: "Maria",
+    apellido: "García",
+  },
+  {
+    nombre: "Ana",
+    apellido: "Rodríguez",
+  },
+  {
+    nombre: "Luis",
+    apellido: "López",
+  },
+]; /* es un arreglo */
+
 /* crea la aplicacion */
-const app=Vue.createApp({
-    /* vue trabaja a travez de opciones con funcionalidades definidas: options api*/
-    /* options api: son conjunto de opciones 
+const app = Vue.createApp({
+  /* vue trabaja a travez de opciones con funcionalidades definidas: options api*/
+  /* options api: son conjunto de opciones 
     que tienen una funcionalidad determinada 
     o una funcionalidad específica.   se usan back tics  ` `*/
-    template: `
+  /* template: `
     <h1>Hola mundo desde Vue.js </h1>
     <p>{{1+1}}</p>
+    <p>{{[1,2,3,4,5]}}</p> 
     <p>{{false?'Verdadero':'Falso'}}</p>
-    `
-    /* {{}}: programacion declarativa. Se pone adentro el codigo JS */
-}); 
+    ` */
+  /* {{}}: programacion declarativa. Se pone adentro el codigo JS */
 
-app.mount('#myApp'); /* monta la app, con cualquier nombre */
+  /* PROPIEDAD REACTIVA:  */
+  data() {
+    /* data  es una option api */
+    return {
+      mensaje: "hola mundo propiedad reactiva",
+      valor: 7,
+      lista: estudiantes /* mi propiedad reactica
+      es lista */,
+    };
+  },
+  methods: {
+    /* methods es una option api */
+    cambiarTexto() {
+      /* Toda propiedad reactiva que se usa en 
+        codigo JS usa el "this." */
+      this.mensaje = "Texto cambiado";
+    },
+    sumar() {
+      this.valor = this.valor + 100;
+    },
+    agregar() {
+      const nuevo = {
+        nombre: "daniel",
+        apellido: "Llumiquinga",
+      };
+      this.lista.unshift(nuevo);
+    },
+  },
+});
+
+app.mount("#myApp"); /* monta la app, con cualquier nombre */
